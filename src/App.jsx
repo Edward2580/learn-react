a278c603190b86423b86d7cd31148f8bb7513c30
Edward ramirez <edwardramirez199@gmail.com>
2025-02-28 21:39:20 -0700

    simple commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        {/* some simple commit */}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
